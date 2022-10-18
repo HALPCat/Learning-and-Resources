547c961dc519fca068bc5320154ee0102d0d95b4
HALPCat <halpcat@hotmail.com>
2022-10-19 02:00:21 +0300

    added weighted dice

---
+++ Assets/Dice/Scripts/LandFacingUpward.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dice
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class LandFacingUpward : MonoBehaviour
+    {
+        public LayerMask layerMask;
+        public float maxDistance = 5f;
+        public Vector3 up = Vector3.up;
+        public float Kp = 100;
+        public float Kd = 20;
+
+        Rigidbody _rb;
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            if(Physics.Raycast(_rb.position, Physics.gravity, out RaycastHit hitInfo, maxDistance, layerMask))
+            {
+                Quaternion.FromToRotation(_rb.rotation * up, hitInfo.normal).ToAngleAxis(out float angle, out Vector3 axis);
+                Vector3 err = Mathf.Deg2Rad * angle * axis;
+                _rb.AddTorque(Kp * err - Kd * _rb.angularVelocity, ForceMode.Acceleration);
+            }
+        }
+    }
+}
---
+++ Assets/Dice/Scripts/RbExplode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dice
+{
+    public class RbExplode : MonoBehaviour
+    {
+        public float explosionForce;
+        Rigidbody _rb;
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _rb.AddExplosionForce(explosionForce, transform.position + Random.insideUnitSphere, .5f, 1f, ForceMode.Impulse);
+            _rb.AddTorque(Random.Range(0f, 1f) * explosionForce, Random.Range(0f, 1f) * explosionForce, Random.Range(0f, 1f) * explosionForce, ForceMode.Impulse);
+        }
+    }
+}
